f1eaf209ee9f513b055fdb3d99ce916e222e4a86
lenovo <ozgurefe@musetechs.com>
2021-02-27 22:08:15 +0300

    Firebase sdk'larını ekledim.

--- app/src/main/java/com/example/mars/util/BindingAdapter.kt
+++ app/src/main/java/com/example/mars/util/BindingAdapter.kt
@@ -9,7 +9,6 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
-import com.squareup.picasso.Picasso
 
 
 object BindingAdapter {
